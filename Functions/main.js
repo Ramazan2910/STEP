8e3d9a8bd80fa22a086e170e0edc182e129097ea
ramazan_quliyev <ramazanquliyev05@gmail.com>
2025-06-25 22:51:13 +0400

    Added Functions Homework

---
+++ Functions/main.js
@@ -0,0 +1,23 @@
+
+/* Task 1 */
+const sum = (num1,num2) => num1+num2;
+
+console.log(`Sum: ${sum(5,4)}`)
+
+/* Task 2 */
+
+const sayHi = (name) => `Hi, ${name}`;
+
+console.log(sayHi("Ramazan"))
+
+/* Task 3 */
+
+const getEvenOdd = (num) => num%2==0?"even":"odd";
+
+console.log(getEvenOdd(4));
+
+/* Task 4 */
+
+const square = (num) => num**2;
+
+console.log(square(5));
