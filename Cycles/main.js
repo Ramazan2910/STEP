1053f81b595620abe45b94407a2f92875db2bdfc
ramazan_quliyev <ramazanquliyev05@gmail.com>
2025-06-25 21:47:36 +0400

    Added Homework

---
+++ Cycles/main.js
@@ -0,0 +1,37 @@
+/* Task 1 */
+
+// console.log("Task 1");
+
+// let n = 159;
+// let sum = 0;
+
+// for(let i = 0; i <= n; i++){
+//     sum+=i;
+// }
+
+// console.log('Sum:',sum);
+
+/* Task 2 */
+
+// console.log("Task 2");
+
+// let m = 3;
+// let n = 15;
+// let result = 1;
+
+// for(let i = m; i<=n;i++){
+//     if(i%2!==0)
+//         result*=i;
+// }
+
+// console.log("Result:", result);
+
+/* Task 3 */
+
+// for(let i = 1; i <=5; i++){
+//     console.log(`###${i}###\n\n`);
+//     for(let j =1; j<=10; j++){
+//         console.log(i + ' x ' + j + ' = '+ i*j);
+//     }
+//     console.log('\n');
+// }
